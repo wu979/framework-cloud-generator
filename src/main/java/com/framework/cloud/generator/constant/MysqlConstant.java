db072475b0b355e24f3a54802e9c321a929f8c1d
吴思伟 <18683789594@163.com>
2022-08-30 15:53:37 +0800

    commit

--- src/main/java/com/framework/cloud/generator/constant/MysqlConstant.java
+++ src/main/java/com/framework/cloud/generator/constant/MysqlConstant.java
@@ -10,15 +10,15 @@ public class MysqlConstant {
     /** 数据库-前缀 */
     public static final String URL_PREFIX = "jdbc:mysql://";
     /** 数据库-地址 */
-    public static final String URL = "139.155.11.84:3306/";
+    public static final String URL = "127.0.0.1:3306/";
     /** 数据库-库名 */
     public static final String DATABASE = "framework-pay";
     /** 数据库-后缀 */
     public static final String URL_SUFFIX = "?characterEncoding=utf8&useSSL=false&connectTimeout=1000&socketTimeout=3000&autoReconnect=true&serverTimezone=GMT%2B8";
     /** 数据库-账号 */
-    public static final String USERNAME = "979";
+    public static final String USERNAME = "root";
     /** 数据库-密码 */
-    public static final String PASSWORD = "wsw979";
+    public static final String PASSWORD = "123456";
     /** 数据库-驱动 */
     public static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
     /** 类型转换-tinyint to int */
